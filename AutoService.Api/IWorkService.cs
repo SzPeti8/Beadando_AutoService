b9ba821def86a812ad99164d8490fc42f6ae3979
SzPeti8 <petirefi11@gmail.com>
2025-05-10 17:13:00 +0200

    Add: Added IWorkService to api

---
+++ AutoService.Api/IWorkService.cs
@@ -0,0 +1,17 @@
+﻿using AutoService.Shared;
+
+namespace AutoService.Api
+{
+    public interface IWorkService
+    {
+        void Add(Work work);
+
+        void Delete(string id);
+
+        List<Work> Get();
+
+        Work Get(string id);
+
+        void Update( Work work);
+    }
+}
