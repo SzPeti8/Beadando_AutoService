e1c38441d397a07594e2906246daf6b9fa8bd731
SzPeti8 <petirefi11@gmail.com>
2025-05-10 15:58:34 +0200

    Add: Addded a shared class library

--- AutoService.Api/Program.cs
+++ AutoService.Api/Program.cs
@@ -1,22 +1,44 @@
+
+using AutoService.Api;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddSerilog(
+    options => options
+        .MinimumLevel.Information()
+        .WriteTo.Console());
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<DemoDataContext>(
+    options =>
+    {
+        options.UseSqlite(builder.Configuration.GetConnectionString("SQLite"));
+        options.UseLazyLoadingProxies();
+    });
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IPersonService, PersonService>();
+
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
-app.UseAuthorization();
+app.UseHttpsRedirection();
 
 app.MapControllers();
 
