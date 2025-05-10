b99e7e9f142efb2b24c2e7055b49575f3eb61150
SzPeti8 <petirefi11@gmail.com>
2025-05-10 16:27:37 +0200

    Add: Added Customer class to shared library

---
+++ AutoService.Shared/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoService.Shared
+{
+    public class Customer
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Adress { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+    }
+}
