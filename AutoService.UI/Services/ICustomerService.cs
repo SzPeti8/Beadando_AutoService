a1835a31548242f33e7c0e93041891f505402ddf
SzPeti8 <petirefi11@gmail.com>
2025-05-12 21:26:44 +0200

    Add: Added ICustomerService to UI

---
+++ AutoService.UI/Services/ICustomerService.cs
@@ -0,0 +1,17 @@
+﻿using AutoService.Shared;
+
+namespace AutoService.UI.Services
+{
+    public interface ICustomerService
+    {
+        Task<IList<Customer>> GetAllCustomersAsync(bool includeWorks = false);
+
+        Task<Customer> GetCustomerAsync(string id, bool includeWorks = false);
+
+        Task DeleteCustomerAsync(string id);
+
+        Task AddCustomerAsync(Customer customer);
+
+        Task UpdateCustomerAsync(Customer customer);
+    }
+}
