eb09321d515dedbdf6e0e715bd58f0ecd5b5efc8
SzPeti8 <petirefi11@gmail.com>
2025-05-12 22:13:29 +0200

    Add: WorkService

---
+++ AutoService.UI/Services/Implementation/WorkService.cs
@@ -0,0 +1,42 @@
+﻿using AutoService.Shared;
+using System;
+using System.Net.Http.Json;
+
+namespace AutoService.UI.Services.Implementation
+{
+    public class WorkService : IWorkService
+    {
+
+        private readonly HttpClient _httpClient;
+
+        public WorkService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task AddWorkAsync(Work work)
+        {
+            await _httpClient.PostAsJsonAsync("/works", work);
+        }
+
+        public async Task DeleteWorkAsync(string id)
+        {
+            await _httpClient.DeleteAsync($"/works/{id}");
+        }
+
+        public async Task<IList<Work>> GetAllWorksAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<IList<Work>>("/works?");
+        }
+
+        public async Task<Work> GetWorkAsync(string id)
+        {
+            return await _httpClient.GetFromJsonAsync<Work>($"/api/Works/{id}?");
+        }
+
+        public async Task UpdateWorkAsync(Work work)
+        {
+            await _httpClient.PutAsJsonAsync($"/works/{work.Id}", work);
+        }
+    }
+}
