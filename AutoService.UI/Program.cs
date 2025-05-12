b3718f5aef500dc3c2c93c7fcf8977889497c724
SzPeti8 <petirefi11@gmail.com>
2025-05-12 22:20:01 +0200

    Add: Added to program.cs as scope the interfaces and their implementations

--- AutoService.UI/Program.cs
+++ AutoService.UI/Program.cs
@@ -1,11 +1,16 @@
 using AutoService.UI;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using AutoService.UI.Services;
+using AutoService.UI.Services.Implementation;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:8080") });
+
+builder.Services.AddScoped<ICustomerService, CustomerService>();
+builder.Services.AddScoped<IWorkService, WorkService>();
 
 await builder.Build().RunAsync();
